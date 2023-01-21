c027ec5a3db03e5e31e4173ab1712845453a6027
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-01-21 22:52:17 +0600

    added alert message for any kind of error in input value

--- js/script.js
+++ js/script.js
@@ -6,12 +6,44 @@ function makeEmpty(){
     document.getElementById("user__product__price").value="";
 }
 
+function isUniqueID(id){
+    for(let i = 0; i < products.length; i++) {
+        if(products[i].product__id === id)  return 1;
+    }
+    return 0;
+}
+
 function addUserItem(){
     const product__id = document.getElementById("user__product__id").value;
     const product__name = document.getElementById("user__product__name").value;
     const product__price = document.getElementById("user__product__price").value;
 
     makeEmpty();
+
+    // Unique ID checking
+    if(isUniqueID(product__id)){
+        window.alert(`${product__id} is used for another product ID`);
+        return;
+    }
+
+    // Removing extra spaces from the product name 
+    product__name = product__name.trim();
+
+    // length of product name checking
+    if(product__name.length > 60){
+        window.alert(`${product__name} is too large to use for as product name`);
+        return;
+    }
+
+    // product price checking
+    if(product__price < 0) {
+        window.alert(`Negative value of any product is impossible`);
+        return;
+    }
+    if(product__price > 100000){
+        window.alert(`${product__price} is huge amount for any product`);
+        return;
+    }
 
 
     products.push({product__id, product__name, product__price});
